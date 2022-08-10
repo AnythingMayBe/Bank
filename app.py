ffac2366f13be6178ea2d85772105e5c155c2464
AnythingMayBe <anythingmaybe@protonmail.com>
2022-08-10 15:22:53 +0200

    Added saveJson functionnality

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 import json
+import shutil
+import time
+import threading
 
 class Application:
     def __init__(self):
@@ -15,11 +18,16 @@ class Application:
             file.close()
 
     def saveJson(self):
-        pass
+        while True:
+            shutil.copyfile("db.json", "backups/" + str(time.time()))
+            with open("db.json", "w") as file:
+                file.write(json.dumps(self.db))
+                file.close()
+            time.sleep(360)
 
 if __name__ == '__main__':
     app = Application()
     app.loadJson()
     app.routes()
-    
+    threading.Thread(target=app.saveJson).start()
     app.app.run()
