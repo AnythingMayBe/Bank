74ec65228a258a5e524955f5a1444a37a0254553
AnythingMayBe <anythingmaybe@protonmail.com>
2022-08-10 15:47:10 +0200

    Added genToken functionnality

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import shutil
 import time
 import threading
 
+# Token generator
+from os import urandom
+from struct import unpack
+from hashlib import sha512
+
 class Application:
     def __init__(self):
         self.app = Flask(__name__)
@@ -23,7 +28,15 @@ class Application:
             with open("db.json", "w") as file:
                 file.write(json.dumps(self.db))
                 file.close()
-            time.sleep(360)
+            time.sleep(5)
+
+    def genToken(self, amount : int):
+        r = urandom(4)
+        u = str(unpack("i", r)[0])
+        hash = sha512()
+        hash.update(u.encode())
+        self.db["tokens"][str(hash.hexdigest())] = amount
+        return str(hash.hexdigest())
 
 if __name__ == '__main__':
     app = Application()
